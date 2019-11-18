aa9485dd33987c49d2eab05866d484fb68b7ec95
rogilmore <thatrickgilmore@gmail.com>
2019-11-18 13:55:38 -0500

    Add combined QA report--gather-clean-both-tasks.Rmd and associated rendering function

--- analysis/R/batch_qa.R
+++ analysis/R/batch_qa.R
@@ -12,6 +12,15 @@ visualize_motion_dur_data <- function(this_fn_full, this_id) {
                     params = list(this_csv_fn = this_fn_full, this_sub_id = this_id))
 }
 
+visualize_both_tasks_data <- function(this_contr_fn_full, this_motion_fn_full, this_id) {
+  rmarkdown::render("analysis/gather-clean-both-tasks.Rmd", 
+                    output_format = "html_document", output_dir = "analysis/qa",
+                    output_file = paste0(this_id, "-both-tasks.html"),
+                    params = list(this_contr_csv_fn = this_contr_fn_full,
+                                  this_motion_dur_csv_fn = this_motion_fn_full,
+                                  this_sub_id = this_id))
+}
+
 extract_ids_from_fns <- function(df_path = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/contrast_sensitivity_task_data") {
   fn_only <- list.files(paste0(df_path), pattern = "\\.csv$")
   stringr::str_sub(fn_only, 1, 16)
@@ -41,6 +50,28 @@ visualize_all_computer_task_data <- function(path_2_data = "~/Box\ Sync/Project_
 
 copy_qa_rpts_to_box <- function(path_2_data = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data") {
   qa_files <- list.files("analysis/qa", pattern = "\\.html$", full.names = TRUE)
-  file.copy(from = qa_files, to = paste0(path_2_data, "/qa_rpts/."))
+  n_copied <- file.copy(from = qa_files, to = paste0(path_2_data, "/qa_rpts/."), overwrite = TRUE)
+  message("Copied ", sum(n_copied), " files to Box.")
 }
 
+visualize_all_combined_tasks <- function(path_2_data = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data") {
+  
+  # paths to data files
+  contr_path <- paste0(path_2_data, "/contrast_sensitivity_task_data")
+  motion_path <- paste0(path_2_data, "/motion_temporal_threshold_data")
+  
+  # Extract IDs
+  contr_ids <- extract_ids_from_fns(contr_path)
+  motion_ids <- extract_ids_from_fns(motion_path)
+  
+  if (length(contr_ids) == length(motion_ids)) {
+    message("Same number of CSV files available for both tasks.")
+    ids_identical <- (contr_ids == motion_ids)
+    if (sum(ids_identical) == length(ids_identical)) {
+      message("All subject IDs match.")
+      contr_fn <- list_full_fns_in_path(contr_path)
+      motion_fn <- list_full_fns_in_path(motion_path)
+      mapply(visualize_both_tasks_data, contr_fn, motion_fn, contr_ids)
+    }
+  }
+}
