eed2ef14b75a09e8a132f8d22736fd7403ac2b2d
rogilmore <thatrickgilmore@gmail.com>
2019-11-21 16:43:13 -0500

    add detect_duplicate_ids(); add assertions to batch_qa functions.

--- analysis/R/batch_qa.R
+++ analysis/R/batch_qa.R
@@ -1,4 +1,9 @@
 visualize_contr_sens_data <- function(this_fn_full, this_id) {
+  
+  assertthat::is.string(this_fn_full)
+  assertthat::has_extension(this_fn_full, "csv")
+  assertthat::is.string(this_id)
+  
   rmarkdown::render("analysis/gather-clean-contrast-sensitivity.Rmd", 
                     output_format = "html_document", output_dir = "analysis/qa",
                     output_file = paste0(this_id, "-contr-sens.html"),
@@ -6,6 +11,11 @@ visualize_contr_sens_data <- function(this_fn_full, this_id) {
 }
 
 visualize_motion_dur_data <- function(this_fn_full, this_id) {
+  
+  assertthat::is.string(this_fn_full)
+  assertthat::has_extension(this_fn_full, "csv")
+  assertthat::is.string(this_id)
+  
   rmarkdown::render("analysis/gather-clean-motion-dur.Rmd", 
                     output_format = "html_document", output_dir = "analysis/qa",
                     output_file = paste0(this_id, "-motion-dur.html"),
@@ -22,33 +32,56 @@ visualize_both_tasks_data <- function(this_contr_fn_full, this_motion_fn_full, t
 }
 
 extract_ids_from_fns <- function(df_path = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/contrast_sensitivity_task_data") {
+  
+  assertthat::is.string(df_path)
+  assertthat::is.dir(df_path)
+  
   fn_only <- list.files(paste0(df_path), pattern = "\\.csv$")
   stringr::str_sub(fn_only, 1, 14)
 }
 
 list_full_fns_in_path <- function(df_path = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/contrast_sensitivity_task_data") {
+  
+  assertthat::is.string(df_path)
+  assertthat::is.dir(df_path)
+  
   fns <- list.files(paste0(df_path), pattern = "\\.csv$", full.names = TRUE)
   fns
 }
 
 visualize_all_contr_sens_data <- function(df_p = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/contrast_sensitivity_task_data") {
+  
+  assertthat::is.string(df_p)
+  assertthat::is.dir(df_p)
+  
   fns <- list_full_fns_in_path(df_p)
   ids <- extract_ids_from_fns(df_p)
   mapply(visualize_contr_sens_data, fns, ids)
 }
 
 visualize_all_motion_dur_data <- function(df_p = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/motion_temporal_threshold_data") {
+
+  assertthat::is.string(df_p)
+  assertthat::is.dir(df_p)
+  
   fns <- list_full_fns_in_path(df_p)
   ids <- extract_ids_from_fns(df_p)
   mapply(visualize_motion_dur_data, fns, ids)
 }
 
 visualize_all_computer_task_data <- function(path_2_data = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data") {
+  assertthat::is.string(path_2_data)
+  assertthat::is.dir(path_2_data)
+  
   visualize_all_contr_sens_data(paste0(path_2_data, "/contrast_sensitivity_task_data"))
   visualize_all_motion_dur_data(paste0(path_2_data, "/motion_temporal_threshold_data"))
 }
 
 copy_qa_rpts_to_box <- function(path_2_data = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data") {
+
+  assertthat::is.string(path_2_data)
+  assertthat::is.dir(path_2_data)
+  
   qa_files <- list.files("analysis/qa", pattern = "\\.html$", full.names = TRUE)
   n_copied <- file.copy(from = qa_files, to = paste0(path_2_data, "/qa_rpts/."), overwrite = TRUE)
   message("Copied ", sum(n_copied), " files to Box.")
@@ -56,6 +89,12 @@ copy_qa_rpts_to_box <- function(path_2_data = "~/Box\ Sync/Project_Sex_differenc
 
 list_common_participant_ids <- function(contr_path = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/contrast_sensitivity_task_data", 
                                         motion_path = "~/Box\ Sync/Project_Sex_difference_on_Motion_Perception/data/motion_temporal_threshold_data") {
+  
+  assertthat::is.string(contr_path)
+  assertthat::is.string(motion_path)
+  assertthat::is.dir(contr_path)
+  assertthat::is.dir(motion_path)
+  
   contr_ids <- extract_ids_from_fns(contr_path)
   motion_ids <- extract_ids_from_fns(motion_path)
   if (length(contr_ids) > length(motion_ids)) {
@@ -102,10 +141,16 @@ list_common_participant_ids <- function(contr_path = "~/Box\ Sync/Project_Sex_di
 
 
 has_enough_trials <- function(data_df, n_trials = 4*30) {
+  assertthat::not_empty(data_df)
+  assertthat::is.number(n_trials)
+  
   dim(data_df)[1] >= n_trials
 }
 
 observer_id_in_file <- function(data_df, task='contr') {
+  assertthat::not_empty(data_df)
+  assertthat::is.string(task)
+  
   if (task=='contr') {
     sum(!(is.na(data_df$Participant))) == length(data_df$Participant)
   } else {
@@ -114,6 +159,9 @@ observer_id_in_file <- function(data_df, task='contr') {
 }
 
 gender_in_file <- function(data_df, task = 'contr') {
+  assertthat::not_empty(data_df)
+  assertthat::is.string(task)
+  
   if (task == 'contr') {
     sum(!(is.na(data_df$Gender))) > 0    
   } else {
@@ -122,7 +170,12 @@ gender_in_file <- function(data_df, task = 'contr') {
 }
 
 sub_id_matches_fn <- function(data_fn, task='contr') {
+  assertthat::is.string(task)
+  assertthat::is.string(data_fn)
+  
   data_df <- readr::read_csv(data_fn)
+  assertthat::not_empty(data_df)
+  
   if (task=='contr') {
     sub_id <- as.character(data_df$Participant)
   } else {
@@ -133,6 +186,10 @@ sub_id_matches_fn <- function(data_fn, task='contr') {
 } 
 
 essential_vars_in_file <- function(data_df, task = 'contr') {
+  
+  assertthat::not_empty(data_df)
+  assertthat::is.string(task)
+  
   if (task == 'contr') {
     essential_vars <- c("correctAns", "loop_trial.intensity", "loop_trial.thisN",
                                             "Participant", "Gender", "resp.rt")
@@ -144,8 +201,17 @@ essential_vars_in_file <- function(data_df, task = 'contr') {
 }
 
 make_qa_df <- function(data_fn, task = 'contr') {
+
+  assertthat::is.string(task)
+  assertthat::is.string(data_fn)
+  assertthat::has_extension(data_fn, "csv")  
+  assertthat::is.readable(data_fn)
+
   fn <- basename(data_fn)
   data_df <- readr::read_csv(data_fn)
+  
+  assertthat::not_empty(data_df)
+  
   data.frame(task = task, fn,
              id_matches_fn = sub_id_matches_fn(data_fn, task = task),
              id_in_file = observer_id_in_file(data_df, task = task),
@@ -156,6 +222,7 @@ make_qa_df <- function(data_fn, task = 'contr') {
 }
 
 run_session_qa_report <- function() {
+  
   rmarkdown::render("analysis/session_qa.Rmd", 
                     output_format = "html_document", output_dir = "analysis/qa",
                     output_file = paste0(Sys.Date(), "-qa-report.html"),
@@ -167,5 +234,9 @@ run_session_qa_report <- function() {
 }
 
 generate_pid <- function(){
-  paste0(lubridate::yday(Sys.Date()), format(Sys.time(), "%H%M%S"))
+  id <- paste0(lubridate::yday(Sys.Date()), format(Sys.time(), "%H%M%S"))
+  
+  assertthat::is.string(id)
+  
+  id
 }
